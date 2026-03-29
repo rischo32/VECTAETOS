bf342e068c681e92934ac612588000343981aa32
Richard Fonfára <17224380+rischo32@users.noreply.github.com>
2026-03-29 19:25:05 +0200

    Create test_transform_immutability.py
    Signed-off-by: Richard Fonfára <17224380+rischo32@users.noreply.github.com>

---
+++ tests/test_transform_immutability.py
@@ -0,0 +1,19 @@
+from implementation import TRANSFORMS
+
+def test_transform_immutability():
+
+    assert isinstance(TRANSFORMS, tuple), "TRANSFORMS must be tuple"
+
+    original_ids = tuple(id(t) for t in TRANSFORMS)
+
+    try:
+        TRANSFORMS += (lambda x: x,)
+        mutated = True
+    except:
+        mutated = False
+
+    assert mutated is False, "TRANSFORMS must be immutable"
+
+    after_ids = tuple(id(t) for t in TRANSFORMS)
+
+    assert original_ids == after_ids, "TRANSFORMS changed"
